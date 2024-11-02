ce3fb68e94d798d21f1398c1354035d50bab2c54
Dharshini-457 <dharshinir619@gmail.com>
2024-11-02 20:36:20 +0530

    portfolio.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
+from PIL import Image
+st.markdown("<style>body{background-color:#333;}</style>",unsafe_allow_html=True)
+image=Image.open("file:///C:/Users/dhars/Downloads/KRS_7411.webp")
+st.image(image,caption='Murugar',)
 st.title("My Portfolio!!")
 
 name = st.text_input("Enter your name:")
 st.write(f"Hi!!{name}")
-if name=="Dharshini":
+if name == "Dharshini":
     st.title("Your prortfolio is")
     btn = st.button("view education!!")
     if btn:
@@ -12,9 +16,9 @@ if name=="Dharshini":
         "year:":[2024,2028],
         "marks":[524,9.5]}
         st.dataframe(dict)
-        btn = st.button(" view project")
-        if btn:
-            st.write("**personal diary")
-else:
-    st.write("Your are not dharshini")
-    
+    btn = st.button(" view project")
+    if btn:
+        st.title("My projects")
+       
+        project=["*Personal Diary","*Todo Game"]
+        st.dataframe(project)
