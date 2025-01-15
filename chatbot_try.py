83c8291fff2dbe9ba82f9666fd9d9856ada784e0
Dharshini-457 <dharshinir619@gmail.com>
2025-01-15 13:59:45 +0530

    resent commit

--- chatbot_try.py
+++ chatbot_try.py
@@ -3,5 +3,5 @@ import spacy
 nlp=spacy.load('en_core_web_sm')
 text=nlp(u"I am learning how to build chatbots")
 for i in text:
-    print(f"(i.text,i.pos_,{|},i.lemma_,{|},,i.shape_,{|},i.is_alpha,{|},i.is_stop)")
+    print(f"(i.text,i.pos_,i.lemma_,i.shape_,i.is_alpha,i.is_stop)")
 print("Spacy is good")
